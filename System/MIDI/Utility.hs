f5bd6914ee339f9928c1f67881e0a465cc68cd8b
Balazs Komuves <bkomuves@gmail.com>
2013-10-09 18:35:08 +0000

    cosmetic changes in Utility.hs

--- System/MIDI/Utility.hs
+++ System/MIDI/Utility.hs
@@ -15,6 +15,8 @@ import Data.List
 import Control.Monad
 import Control.Concurrent
 
+import System.IO
+
 import System.MIDI
 import System.MIDI.Base
 
@@ -29,20 +31,32 @@ maybeRead s = case reads s of
 -- If there is only a single device, we select that.
 -- You can also set a default device (by its name), which
 -- will be automatically selected if present.
-selectMidiDevice :: MIDIHasName a => Maybe String -> [a] -> IO a  
-selectMidiDevice mbdefault srclist = do
+selectMidiDevice 
+  :: MIDIHasName a 
+  => String         -- ^ prompt
+  -> Maybe String   -- ^ default device name
+  -> [a]            -- ^ list of devices
+  -> IO a  
+selectMidiDevice prompt mbdefault srclist = do
   names <- mapM getName srclist
-  forM_ (zip [1..] names) $ \(i,name) -> putStrLn $ show i ++ ": " ++ name
   let nsrc = length srclist
+  putStrLn prompt
   src <- case srclist of
-    []  -> fail "no midi devices found"
-    [x] -> return x
+    []  -> do
+      putStrLn "no midi devices found"
+      fail "no midi devices found"
+    [x] -> do
+      putStrLn $ "device #1 (" ++ head names ++ ") selected."
+      return x
     _  -> do
       k <- case findIndex (==mbdefault) (map Just names) of
         Just i -> return (i+1)
         Nothing -> do
-          putStrLn "please select a midi device"
+          forM_ (zip [1..] names) $ \(i,name) -> putStrLn $ show i ++ ": " ++ name
+          putStr "please select a midi device: "
+          hFlush stdout
           l <- getLine
+          putStrLn ""
           let k = case maybeRead l of
                     Nothing -> nsrc
                     Just m  -> if m<1 || m>nsrc then nsrc else m
@@ -52,19 +66,17 @@ selectMidiDevice mbdefault srclist = do
   return src
   
 -- | Select a MIDI input device (source) 
-selectInputDevice :: Maybe String -> IO Source  
-selectInputDevice mbdefault = do
+selectInputDevice :: String -> Maybe String -> IO Source  
+selectInputDevice prompt mbdefault = do
   srclist <- enumerateSources
-  putStrLn "midi sources:"
-  src <- selectMidiDevice mbdefault srclist
+  src <- selectMidiDevice prompt mbdefault srclist
   return src
   
 -- | Select a MIDI output device (destination)
-selectOutputDevice :: Maybe String -> IO Destination  
-selectOutputDevice mbdefault = do
+selectOutputDevice :: String -> Maybe String -> IO Destination  
+selectOutputDevice prompt mbdefault = do
   dstlist <- enumerateDestinations
-  putStrLn "\nmidi destinations:"
-  dst <- selectMidiDevice mbdefault dstlist
+  dst <- selectMidiDevice prompt mbdefault dstlist
   return dst
 
 --------------------------------------------------------------------------------
