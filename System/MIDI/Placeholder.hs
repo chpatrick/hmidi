2124a85823cddea36239c59955740aa9012249dd
Patrick Chilton <chpatrick@gmail.com>
2014-01-20 20:34:48 +0000

    Fix placeholder.

--- System/MIDI/Placeholder.hs
+++ System/MIDI/Placeholder.hs
@@ -35,6 +35,9 @@ module System.MIDI.Placeholder
 import Data.Word
 import System.MIDI.Base
 
+placeholder :: a
+placeholder = error "Placeholder value for Haddock."
+
 -- |The opaque data type representing a MIDI source.
 data Source 
 
@@ -46,13 +49,16 @@ data Connection
 
 class MIDIHasName c
 
+instance MIDIHasName Source
+instance MIDIHasName Destination
+
 -- |Enumerates the MIDI sources present in the system.
 enumerateSources :: IO [Source]
-enumerateSources = undefined
+enumerateSources = placeholder
 
 -- |Enumerates the MIDI destinations present in the system.
 enumerateDestinations :: IO [Destination]
-enumerateDestinations = undefined
+enumerateDestinations = placeholder
 
 -- |These functions return the name, model and manufacturer of a MIDI source \/ destination.
 -- 
@@ -61,59 +67,59 @@ getName :: MIDIHasName a => a -> IO String
 getModel :: MIDIHasName a => a -> IO String
 getManufacturer :: MIDIHasName a => a -> IO String
 
-getName = undefined
-getModel = undefined
-getManufacturer = undefined
+getName = placeholder
+getModel = placeholder
+getManufacturer = placeholder
 
 -- |Opens a MIDI Source.
 -- There are two possibilites to receive MIDI messages. The user can either supply a callback function,
 -- or get the messages from an asynchronous buffer. However, mixing the two approaches is not allowed.
 openSource :: Source -> Maybe ClientCallback -> IO Connection 
-openSource = undefined
+openSource = placeholder
 
 -- |Opens a MIDI Destination.
 openDestination :: Destination -> IO Connection 
-openDestination = undefined
+openDestination = placeholder
 
 -- |Gets the next event from a buffered connection (see also `openSource`)
 getNextEvent :: Connection -> IO (Maybe MidiEvent)
-getNextEvent = undefined
+getNextEvent = placeholder
 
 -- | Checks the next event from a buffered connection, but does not remove it from the buffer.
 checkNextEvent :: Connection -> IO (Maybe MidiEvent)
-checkNextEvent = undefined
+checkNextEvent = placeholder
 
 -- | Gets all the events with timestamp less than the specified from the buffer.
 getEventsUntil :: Connection -> TimeStamp -> IO [MidiEvent]
-getEventsUntil = undefined
+getEventsUntil = placeholder
 
 -- |Gets all the events from the buffer (see also `openSource`)
 getEvents :: Connection -> IO [MidiEvent]
-getEvents = undefined
+getEvents = placeholder
       
 -- |Sends a short message. The connection must be a `Destination`.
 send :: Connection -> MidiMessage -> IO ()
-send = undefined
+send = placeholder
 
 -- |Sends a system exclusive message. You shouldn't include the starting \/ trailing bytes 0xF0 and 0xF7.
 -- 
 -- Note: On Win32, the connection must be a `Destination`
 sendSysEx :: Connection -> [Word8] -> IO ()
-sendSysEx = undefined
+sendSysEx = placeholder
  
 -- |Starts a connection. This is required for receiving MIDI messages, and also for starting the clock.
 start :: Connection -> IO ()
-start = undefined
+start = placeholder
 
 -- |Stops a connection.
 stop :: Connection -> IO ()
-stop = undefined
+stop = placeholder
   
 -- |Closes a MIDI Connection.
 close :: Connection -> IO ()
-close = undefined
+close = placeholder
  
 -- |Returns the time elapsed since the last `start` call, in milisecs.
 currentTime :: Connection -> IO Word32
-currentTime = undefined
+currentTime = placeholder
  
